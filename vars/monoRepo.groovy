c2287cd557a2e71a3803b776104ecb60fcd2bbd6
raghunath1986 <39937557+raghunath1986@users.noreply.github.com>
2021-12-23 12:02:28 -0500

    Update monoRepo.groovy

--- vars/monoRepo.groovy
+++ vars/monoRepo.groovy
@@ -3,6 +3,13 @@ def call(body){
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
+    
+  @NonCPS
+    def getUrlParts(def url){
+        urlParts = url =~ /(?<baseUrl>.*)\/(?<repoOwner>.*)\/(?<repository>.*)/
+        return urlParts
+    }
+    
   pipeline {
     agent any
     stages {
@@ -11,12 +18,12 @@ def call(body){
           cleanWs()
           script {
                  // define some variables
-                 /*
-                 def githubUrlParts = GITREPO_URL_HTTPS =~ /(?<baseUrl>.*)\/(?<repoOwner>.*)\/(?<repository>.*)/
+                 
+                 def githubUrlParts = getUrlParts (GITREPO_URL_HTTPS) 
                  env.GITREPO_BASEURL = githubUrlParts[0][1]
                  env.GITREPO_OWNER = githubUrlParts[0][2]
                  env.GITREPO_NAME = githubUrlParts[0][3].replace('.git','')
-                 */           
+                           
                  
                  if (!env.GITREPO_BRANCH) {
                      env.GITREPO_BRANCH = "master"
@@ -25,7 +32,7 @@ def call(body){
                     'svc-githubjira',
                      GITREPO_URL_HTTPS,
                      GITREPO_BRANCH,
-                     GITREPO_NAME
+                     'Jenkins'
                    )
               echo "BaseURL entered is: $GITREPO_URL_HTTPS"
               
