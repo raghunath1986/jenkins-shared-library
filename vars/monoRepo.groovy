0ee9a5d78b25266eb38c515a02f9fca9a1b0bb48
raghunath1986 <39937557+raghunath1986@users.noreply.github.com>
2021-12-23 10:57:40 -0500

    Create monoRepo.groovy

---
+++ vars/monoRepo.groovy
@@ -0,0 +1,32 @@
+def call(body){
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+  pipeline {
+    agent any
+    stages {
+      stage('Prepare Environment'){
+        steps {
+          cleanWs()
+          script {
+                 // define some variables
+                 githubUrlParts = GITREPO_URL_HTTPS =~ /(?<baseUrl>.*)\/(?<repoOwner>.*)\/(?<repository>.*)/
+                 env.GITREPO_BASEURL = githubUrlParts[0][1]
+                 env.GITREPO_OWNER = githubUrlParts[0][2]
+                 env.GITREPO_NAME = githubUrlParts[0][3].replace('.git','')
+                 if (!env.GITREPO_BRANCH) {
+                     env.GITREPO_BRANCH = "master"
+                 }
+                 git.CheckoutRepoHttps(
+                    'svc-githubjira',
+                     GITREPO_URL_HTTPS,
+                     GITREPO_BRANCH,
+                     GITREPO_NAME
+                   )
+       }
+      }
+    }
+  }
+}
+}
